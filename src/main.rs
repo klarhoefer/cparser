284e48ca8ee97338468d55b435d03dbf5428c135
Udo Klarhöfer <klarhoefer@gmail.com>
2021-07-26 18:11:42 +0200

    Refactored Tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
@@ -29,6 +28,10 @@ impl<'a> Tokenizer<'a> {
 
     fn step(&mut self) {
         self.pos += 1;
+    }
+
+    fn step_n(&mut self, n: usize) {
+        self.pos += n;
     }
 
     fn current(&self) -> Option<u8> {
@@ -128,62 +131,50 @@ impl<'a> Iterator for Tokenizer<'a> {
 
             match b {
                 b';' | b',' | b'{' | b'}' | b'[' | b']' | b'(' | b')' | b'?' | b':' => return Some(self.slice()),
-                b'_' | b'a'..=b'z' | b'A'..=b'Z' => return Some(self.identifier()),
-                b'*' => {
-                    match self.peek() {
-                        Some(b'=') => {
+                b'.' => {
+                    match (self.current(), self.peek()) {
+                        (Some(b'.'), Some(b'.')) => self.step_n(2),
+                        _ => (),
+                    }
+                    return Some(self.slice());
+                },
+                b'*' | b'^' | b'!' | b'=' | b'/' => {
+                    match self.current() {
+                        Some(b'=') => self.step(),
+                        _ => (),
+                    }
+                    return Some(self.slice());
+                },
+                b'&' | b'|' | b'+' => {
+                    match self.current() {
+                        Some(b'=') => self.step(),
+                        Some(b2) if b2 == b => self.step(),
+                        _ => (),
+                    }
+                    return Some(self.slice());
+                },
+                b'<' | b'>' => {
+                    match self.current() {
+                        Some(b'=') => self.step(),
+                        Some(b2) if b2 == b => {
                             self.step();
+                            match self.current() {
+                                Some(b'=') => self.step(),
+                                _ => (),
+                            }
                         },
                         _ => (),
                     }
                     return Some(self.slice());
                 },
                 b'-' => {
-                    match self.peek() {
-                        Some(b'>') | Some(b'-') | Some(b'=') => {
-                            self.step();
-                        },
+                    match self.current() {
+                        Some(b'>') | Some(b'-') | Some(b'=') => self.step(),
                         _ => (),
                     }
                     return Some(self.slice());
                 },
-                b'+' => {
-                    match self.peek() {
-                        Some(b'+') | Some(b'=') => {
-                            self.step();
-                        },
-                        _ => (),
-                    }
-                    return Some(self.slice());
-                },
-                b'=' => {
-                    match self.peek() {
-                        Some(b'=') => {
-                            self.step();
-                        },
-                        _ => (),
-                    }
-                    return Some(self.slice());
-                },
-                b'&' => {
-                    match self.peek() {
-                        Some(b'=') | Some(b'&') => {
-                            self.step();
-                        },
-                        _ => (),
-                    }
-                    return Some(self.slice());
-                },
-                b'|' => {
-                    match self.peek() {
-                        Some(b'=') | Some(b'|') => {
-                            self.step();
-                        },
-                        _ => (),
-                    }
-                    return Some(self.slice());
-                },
-                b'.' | b'^' => return Some(self.slice()),
+                b'_' | b'a'..=b'z' | b'A'..=b'Z' => return Some(self.identifier()),
                 b'0'..=b'9' => return Some(self.number()),
                 b'"' => return Some(self.string()),
                 b'\'' => return Some(self.character()),
@@ -194,48 +185,68 @@ impl<'a> Iterator for Tokenizer<'a> {
     }
 }
 
+fn try_parse_struct(stmt: &[String]) {
+    if stmt[0] == "struct" {
+        let l = stmt.len();
+        let mut p = 1;
+        while p < l && stmt[p] != "{" {
+            p += 1;
+        }
+        p += 1;
 
-#[derive(Debug)]
-enum Token {
-    Reserved(String),
-    Identifier(String),
-    Operator(String),
+        while p < l {
+            let mut i = p + 1;
+            while i < l && stmt[i] != ";" {
+                i += 1;
+            }
+            if i < l {
+                println!("\ttype = {:?}", &stmt[p..i]);
+            }
+            p = i + 1;
+        }
+    }
 }
+
+fn parse_statement(stmt: &[String]) {
+    let l = stmt.len();
+    if l > 2 {
+        if stmt[0] == "typedef" {
+            let name = &stmt[l - 2];
+            println!("name = {}", name);
+            try_parse_struct(&stmt[1..l-2]);
+        }
+    }
+}
+
 
 fn parse<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let file = File::open(path)?;
     let reader = io::BufReader::new(file);
 
+    let mut statement = Vec::new();
+    let mut balance = 0;
     for line in reader.lines() {
         let line = line?;
         let trimmed = line.trim();
-        if trimmed.len() == 0 || trimmed.starts_with("#line") {
+        if trimmed.len() == 0 || trimmed.starts_with("#line") || trimmed.starts_with("#pragma") {
             continue;
         }
-
-        if trimmed.starts_with("#pragma") {
-            continue;
-        }
-
-        print!("{} |", line.trim_end());
 
         let tokenizer = Tokenizer::new(&trimmed);
         for token in tokenizer {
-            print!(" ~{}~", token);
-            // let token = match token {
-            //     "typedef" | "struct" | "union" | "for" | "while" | "if" | "extern" | "sizeof" | "enum" |
-            //         "void" | "int" | "double" | "float" | "unsigned" | "char" | "const" | "long" | "short" => Token::Reserved(token.into()),
-            //     "*" | ";" | "!" | "(" | ")" | "{" | "}" | "[" | "]" | "," | "=" => Token::Operator(token.into()),
-            //     _ => Token::Identifier(token.into()),
-            // };
-            // match token {
-            //     Token::Identifier(s) => {
-            //         println!("\"{}\"", s);
-            //     },
-            //     _ => (),
-            // }
+            statement.push(token.to_string());
+            match token {
+                "{" | "[" | "(" => balance += 1,
+                "}" | "]" | ")" => balance -= 1,
+                ";" if balance == 0 => {
+                    parse_statement(&statement);
+                    // let stmt = statement.join(" ");
+                    // println!("~ {}", stmt);
+                    statement.clear();
+                },
+                _ => (),
+            }
         }
-        println!();
 
     }
 
@@ -246,8 +257,23 @@ fn parse<P: AsRef<Path>>(path: P) -> io::Result<()> {
 
 fn main() {
     // file created with cl /P <header file>
-    match parse("hdf5.i") {
+    match parse(r"..\processEsie_HST4.i") {
         Ok(_) => println!("OK"),
         Err(e) => eprintln!("{:?}", e),
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::Tokenizer;
+
+    #[test]
+    fn it_works() {
+        let tokenizer = Tokenizer::new("abc def = == ! != xyz ... < << <<= > >> >>=");
+        for token in tokenizer {
+            println!("{}", token);
+        }
     }
 }
